428acf9a1fbd092627fea858d3faceebdae2d8ff
roderickvella <55999524+roderickvella@users.noreply.github.com>
2024-02-26 11:12:58 +0100

    Lesson3

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.UI;
+using TMPro;
 
 public class InventoryManager : MonoBehaviour
 {
@@ -14,7 +16,17 @@ public class InventoryManager : MonoBehaviour
     [Tooltip("List of Items")]
     public List<ItemScriptableObject> itemsAvailable;
 
+    [Tooltip("Selected Item Colour")]
+    public Color selectedColor;
+
+    [Tooltip("Not Selected Item Colour")]
+    public Color notSelectedColor;
+
+
+
     private List<InventoryItem> itemsForPlayer;
+
+    private int currentSelectedIndex = 0;//by default start/select the first button
 
 
     // Start is called before the first frame update
@@ -23,11 +35,46 @@ public class InventoryManager : MonoBehaviour
         itemsForPlayer = new List<InventoryItem>();
 
         PopulateInventorySpawn();
+        RefreshInventoryGUI();
+    }
+
+    private void RefreshInventoryGUI()
+    {
+        int buttonId = 0;
+        foreach (InventoryItem i in itemsForPlayer)
+        {
+            //load the button
+            GameObject button = itemsSelectionPanel.transform.Find("Button" + buttonId).gameObject;
+
+            //search for the child image and change its sprite
+            button.transform.Find("Image").GetComponent<Image>().sprite = i.item.icon;
+
+            //search for the child quantity and update the text
+            button.transform.Find("Quantity").GetComponent<TextMeshProUGUI>().text = "x" + i.quantity;
+
+            if(buttonId == currentSelectedIndex)
+            {
+                button.GetComponent<Image>().color = selectedColor;
+            }
+            else
+            {
+                button.GetComponent<Image>().color = notSelectedColor;
+            }
+
+            buttonId += 1;
+
+        }
+
+        //set active false redundant buttons
+        for (int i = buttonId; i < 3; i++)
+        {
+            itemsSelectionPanel.transform.Find("Button" + i).gameObject.SetActive(false);
+        }
     }
 
     private void PopulateInventorySpawn()
     {
-        for(int i=0; i<numberOfItems; i++)
+        for (int i = 0; i < numberOfItems; i++)
         {
             //pick random object from itemsAvailable
             ItemScriptableObject objItem = itemsAvailable[Random.Range(0, itemsAvailable.Count)];
@@ -36,7 +83,7 @@ public class InventoryManager : MonoBehaviour
             //we've got of type objItem inside itemsForPlayer
             int countItems = itemsForPlayer.Where(x => x.item == objItem).ToList().Count;
 
-            if(countItems == 0)
+            if (countItems == 0)
             {
                 //add objItem with quantity 1 because it is the first type inside itemsForPlayer
                 itemsForPlayer.Add(new InventoryItem() { item = objItem, quantity = 1 });
@@ -57,8 +104,42 @@ public class InventoryManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.J) || Input.GetKeyDown(KeyCode.K))
+        {
+            ChangeSelection();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            ConfirmSelection();
+        }
     }
+
+    private void ChangeSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            currentSelectedIndex -= 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            currentSelectedIndex += 1;
+        }
+
+        //check boundaries
+        if (currentSelectedIndex < 0)
+            currentSelectedIndex = 0;
+
+        if (currentSelectedIndex == itemsForPlayer.Count)
+            currentSelectedIndex = currentSelectedIndex - 1;
+
+        RefreshInventoryGUI();
+    }
+
+    private void ConfirmSelection()
+    {
+
+    }
+
 
     public class InventoryItem
     {
